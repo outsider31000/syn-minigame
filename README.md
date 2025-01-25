995581cb9aba103c11f8642677182e1990bc110f
Zeus Script <51821404+Z-eus@users.noreply.github.com>
2025-01-25 03:10:25 +0300

    Overall regulation, improvement.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # syn-minigame
-minigame
+
+Minigame script for RedM
+
+## Usage
+```
+local test = exports["syn_minigame"]:taskBar(5000,7) -- Difficulty, SkillGapSent
+print(test)
+```
+
+
+Example
+```
+local minigame = exports["syn_minigame"]:taskBar(5000,7)
+
+print(minigame)
+
+if minigame == 100 then
+	print("Successful")
+else
+	print("Failed")
+end
+````
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,18 @@
+fx_version "adamant"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game "rdr3"
+
 client_script {
 	'client.lua'
 }
-ui_page('index.html')
+
+ui_page('ui/index.html')
 
 files({
-'index.html',
-'scripts.js',
-"css/style.css"
+	'ui/index.html',
+	'ui/scripts.js',
+	"ui/style.css"
 })
-
-fx_version "adamant"
-
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
-game "rdr3"
 
 export "taskBar"
 export "closeGuiFail"
